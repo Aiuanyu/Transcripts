9e5b42e18935ddff4caf99bcb732463df83e44c2
GHSRobert Ciang <Robertus0617@gmail.com>
2023-06-12 09:11:05 +0800

    「緊行緊遠」（待細修、註解）（初試獨立 data-xxx.js）

---
+++ data-gin-hang-gin-rhan.js
@@ -0,0 +1,169 @@
+data = [
+  {
+    "EP": "",
+    "sections": [
+      {
+        "t": "黃子軒「Gín hang gín rhán 緊行緊遠」<br>（客語海陸腔）",
+        "id": "gin-hang-gin-rhan",
+        "rows": [
+          [
+            "",
+            "",
+            "<iframe style='width: 70vw; height: 25vh; position: fixed; z-index: 50;' src='https://www.youtube.com/embed/M4bIdZTDnyw' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe><div style='width: 100%; height: 25vh;'></div>",
+            ""
+          ],
+          [
+            "",
+            "𠊎話著　行出家門<span class='noted'>以</span>後",
+            "<div class='sidenote'>從初學者的角度來說，我只是從網路及書籍上學習語言，比較沒有機會接觸到各地各庄生活中不同的習慣，所以我的專案還是把聽到的和學到的出入一一客觀列出，但不影響對歌曲及歌聲的欣賞。<hr>海陸腔 rhi 的音子軒在這首歌裡多半發音為 i（較屬四縣腔的發音）。</div>Ngai vā dó hang chud gàmun <span class='noted'>rhì</span>hēu",
+            ""
+          ],
+          [
+            "",
+            "<span class='noted'>一</span>路上个風景　都會無共樣",
+            "<span class='noted'>Rhid</span> lū shòng gě fùnggín dū vōi mo kiūngrhōng",
+            ""
+          ],
+          [
+            "",
+            "𠊎話著　背等吉他滿哪<span class='noted'>位</span>走",
+            "<div class='sidenote'><i>vī</i> 較偏四縣腔的音，海陸腔是 vūi</div>Ngai vā dó ba dén gidtà màn nāi<span class='noted'>vūi</span> zéu",
+            ""
+          ],
+          [
+            "",
+            "假使三餐毋食　<span class='noted'>也</span>毋會枵著",
+            "<div class='sidenote'><i>iā</i> 較偏四縣腔的音，海陸腔是 rhā</div>Gásíi sàm còn m shìd <span class='noted'>rhā</span> m vōi iàu dó",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "想像中个路　緊行緊遠　𠊎緊行緊遠",
+            "Sióngsiǒng zhùng gǎi lū gín hang gín rhán ngai gín hang gín rhán",
+            ""
+          ],
+          [
+            "",
+            "頭擺个朋友　緊行緊遠　𠊎緊行緊遠",
+            "Teubái gǎi penrhiù gín hang gín rhán ngai gín hang gín rhán",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "該日𠊎又夢到　高中該兜孤盲仔",
+            "Gai ngid ngai rhiū mūng dǒ gòzhùng gai dèu gòmòer",
+            ""
+          ],
+          [
+            "",
+            "打紙炮打到　教官全部都來哩",
+            "Dá zhípǎu dá dǒ gǎugòn cionpū dū loi lè",
+            ""
+          ],
+          [
+            "",
+            "隔壁个細妹　講愛到臺北做明星",
+            "Gagbiag gǎi sěmǒi góng ǒi dǒ Toibed zǒ minsèn",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "𠊎想跈等姖个路行　嗄離麼个都緊來緊遠",
+            "Ngai sióng ten dén gi gǎi lū hang sǎ li mágǎi dū gín loi gín rhán",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "* 緊行緊遠　緊行緊遠　𠊎緊行緊遠",
+            "* Gín hang gín rhán gín hang gín rhán ngai gín hang gín rhán",
+            ""
+          ],
+          [
+            "",
+            "緊行緊遠　緊行緊遠　𠊎緊行緊遠",
+            "Gín hang gín rhán gín hang gín rhán ngai gín hang gín rhán",
+            ""
+          ],
+          [
+            "",
+            "想像中个路　緊行緊遠　𠊎緊行緊遠",
+            "<div class='sidenote'>海陸腔客族鄉親可能 gǎi、gě 通用，這我需要再查證</div>Sióngsiǒng zhùng <span class='noted'>gǎi</span> lū gín hang gín rhán ngai gín hang gín rhán",
+            ""
+          ],
+          [
+            "",
+            "頭擺个朋友　緊行緊遠　𠊎緊行緊遠 *",
+            "<div class='sidenote'>「客家的卡夫卡」專輯中 live 演唱時這句和前句順序顛倒</div>Teubái gǎi penrhiù gín hang gín rhán ngai gín hang gín rhán *",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "𠊎話著　這隻年歲以後",
+            "Ngai vā dó liá zhag ngiensǒi rhìhēu",
+            ""
+          ],
+          [
+            "",
+            "人生肚个風景　都會無共樣",
+            "Nginsèn dú gǎi fùnggín dù vōi mo kiūngrhōng",
+            ""
+          ],
+          [
+            "",
+            "𠊎話著　背等吉他滿哪位走",
+            "Ngai vā dó ba dén gidtà màn nāivūi zéu",
+            ""
+          ],
+          [
+            "",
+            "假使三餐毋食　也毋會枵著",
+            "Gásíi sàm còn m shìd rhā m vōi iàu dó",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "",
+            "*",
+            ""
+          ]
+        ]
+      }
+    ]
+  }
+]
