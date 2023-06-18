c1fd5fbea9794a47ffa418df21df21af39b508f3
GHSRobert Ciang <Robertus0617@gmail.com>
2023-06-18 19:48:25 +0800

    連俞涵參加「客聲獎」宣傳順口溜

---
+++ rhiham.js
@@ -0,0 +1,115 @@
+data = [
+  {
+    "EP": "",
+    "sections": [
+      {
+        "t": "連俞涵為「客聲獎」第 2 屆宣傳+順口溜",
+        "id": "rhiham",
+        "rows": [
+          [
+            "",
+            "",
+            "<iframe src='https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fhakkaradio105.9%2Fvideos%2F1744274219321978%2F&show_text=false&width=560&t=0' width='560' height='314' style='border:none; position: fixed; z-index: 50;' scrolling='no' frameborder='0' allowfullscreen='true' allow='autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share' allowFullScreen='true'></iframe><div style='width: 560px; height: 314px;'></div>",
+            ""
+          ],
+          [
+            "",
+            "連俞涵",
+            "<div class='sidenote'>小吉 👍</div><span class='si'>Liěn Ǐhǎm</span><br><span class='hl'>Lien Rhiham</span>",
+            ""
+          ],
+          [
+            "",
+            "大家好",
+            "<span class='si'>Taigá hò</span><br><span class='hl'>Tāigà hó</span>",
+            ""
+          ],
+          [
+            "",
+            "橘醬、軟軟个<span class='noted'>粢粑</span>",
+            "<div class='sidenote'>Ⓜ️ 麻糬</div><span class='si'>gìdjiong, ngión-ngión ge qǐbá</span><br><span class='hl'>gidziǒng, ngiòn-ngiòn gǎi cibà</span>",
+            ""
+          ],
+          [
+            "",
+            "<span class='noted'>土雞盤</span>、粄粽、菜包",
+            "<div class='sidenote'>Ⓜ️ 白斬雞</div><span class='si'>tùgiépǎn, bànzung, coibáu</span><br><span class='hl'>túgàipan, bánzǔng, cǒibàu</span>",
+            ""
+          ],
+          [
+            "",
+            "（順口溜↓）",
+            "<hr>",
+            ""
+          ],
+          [
+            "",
+            "ㄟ～",
+            "É~",
+            ""
+          ],
+          [
+            "",
+            "𤘅眵毋𤘅奓",
+            "<div class='sidenote'>不正經的人</div><span class='si'>Ngazíi m̌ ngada</span><br><span class='hl'>Ngǎzhì m ngǎdǎ</span>",
+            ""
+          ],
+          [
+            "",
+            "㧡水淋河壩",
+            "<div class='sidenote'>提水倒在河邊</div><span class='si'>Kái sùi lǐm hǒba</span><br><span class='hl'>Kài shúi lim hobǎ</span>",
+            ""
+          ],
+          [
+            "",
+            "若姆講毋怕",
+            "<div class='sidenote'>你媽說沒關係（不怕你媽說的話？）</div><span class='si'>Ngiá mé gòng m̌ pa</span><br><span class='hl'>Ngia mè góng m pǎ</span>",
+            ""
+          ],
+          [
+            "",
+            "該屎胐就打三下",
+            "<div class='sidenote'>屁股就打三下<br>（「胐」為肉部）</div><span class='si'>Ge siivùd qiu dà sám ha</span><br><span class='hl'>Gai shīvud ciū dá sàm hā</span>",
+            ""
+          ],
+          [
+            "",
+            "細人毋曉講客話",
+            "<div class='sidenote'>小孩不會講客語</div><span class='si'>Se'ngǐn m̌ hiàu gòng Hàgfa</span><br><span class='hl'>Sě'ngin m hiáu góng Hagfǎ</span>",
+            ""
+          ],
+          [
+            "",
+            "阿公婆會䟘起來罵",
+            "<div class='sidenote'>祖先會起來罵</div><span class='si'>Ágúngpǒ voi hong hìlǒi ma</span><br><span class='hl'>Āgùngpo vōi hǒng híloi mǎ</span>",
+            ""
+          ],
+          [
+            "",
+            "請同細人講客話",
+            "<div class='sidenote'>請和小孩講客語</div><span class='si'>Qiàng tǔng se'ngǐn gòng Hàgfa</span><br><span class='hl'>Ciáng tung sě'ngin góng Hagfǎ</span>",
+            ""
+          ],
+          [
+            "",
+            "客人愛講客家話",
+            "<div class='sidenote'>客家人要講客家話</div><span class='si'>Hàgngǐn oi gòng Hàggáfa</span><br><span class='hl'>Hagngin ǒi góng Haggàfǎ</span>",
+            ""
+          ],
+          [
+            "",
+            "從細曉講客家話",
+            "<div class='sidenote'>從小會說客家話</div><span class='si'>Qiǔng se hiàu gòng Hàggáfa</span><br><span class='hl'>Ciung sě hiáu góng Haggàfǎ</span>",
+            ""
+          ],
+          [
+            "",
+            "大咧頭腦就精呱呱啊、精呱呱！",
+            "<div class='sidenote'>長大頭腦就頂呱呱啊、頂呱呱！</div><span class='si'>Tai lè těunò qiu jín-guà-guǎ a, jín-guà-guǎ!</span><br><span class='hl'>Tāi lè teunó ciū zìn-guá-gua ā, zìn-guá-gua!</span>",
+            ""
+          ]
+        ]
+      }
+    ]
+  }
+]
