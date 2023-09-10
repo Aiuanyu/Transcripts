276382fa3c20764ad2cc46a8db3233b6fb642686
GHSRobert Ciang <Robertus0617@gmail.com>
2023-09-10 17:03:28 +0800

    「路 2024」
    嘗試不用 names.js

---
+++ lu-2024.js
@@ -0,0 +1,261 @@
+data = [
+  {
+    "EP": "",
+    "sections": [
+      {
+        "t": "Lōo / Lu / 路 2024<br><br>詞 / 曲 / 唱<br>謝銘祐（🟢 鶴佬）、黃珮舒（🔵 客）",
+        "id": "lu-2024",
+        "rows": [
+          [
+            "",
+            "",
+            "<iframe style='width: 70vw; height: 25vh; position: fixed; z-index: 50;' src='https://www.youtube.com/embed/UpzLpfJ027k' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe><div style='width: 100%; height: 25vh;'></div>",
+            ""
+          ],
+          [
+            "耆",
+            "這是我們的山",
+            "<ruby>O<rt>名詞類別標記</rt></ruby> <ruby>lotok<rt>山</rt></ruby> <ruby>no<rt>屬格</rt></ruby> <ruby>mita<rt>我們 [屬格]</rt></ruby> <ruby>konian<rt>這 [主格] -an</rt></ruby>",
+            "3"
+          ],
+          [
+            "",
+            "這是我們的海",
+            "O <ruby>riyar<rt>海</rt></ruby> no mita konian",
+            "3"
+          ],
+          [
+            "",
+            "我們的家是我們的土地",
+            "O <ruby>sera<rt>土地</rt></ruby> no mita ko <ruby>kamoroan<rt><span class='TBD'>家</span></rt></ruby> no mita<div class='sidenote'>查不到 kamoroan，只有一個叫 Kamoro'an 的品牌說是「住下來吧」的意思，所以只好推測 kamoroan 是「家」之意？</div>",
+            "3"
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "Tâi",
+            "有路　咱沿路唱歌",
+            "Ū lōo lán iân-lōo tshiùnn-kua",
+            "2"
+          ],
+          [
+            "",
+            "無路　咱蹽溪過嶺",
+            "Bô lōo lán liâu khe kuè niá",
+            "2"
+          ],
+          [
+            "",
+            "時間　伊從來毋捌回頭行",
+            "Sî-kan i tsiông-lâi m̄-bat huê-thâu kiânn",
+            "2"
+          ],
+          [
+            "",
+            "親像咧叫我个 × 2",
+            "Tshin-tshiūnn leh kiò guá ê × 2",
+            "2"
+          ],
+          [
+            "",
+            "愛一步　一步",
+            "Ài tsi̍t pōo tsi̍t pōo",
+            "2"
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "原",
+            "（吟唱音節，無意思）",
+            "* Hai-yan ho-hai-yan<br>Hai-yan hai-yan<br>I-ya-ho hai-yan",
+            "3"
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "Tâi",
+            "路　欲<ruby>按<rt>焉</rt></ruby>怎行",
+            "Lōo beh án-tsuánn kiânn",
+            "2"
+          ],
+          [
+            "",
+            "頭前　茫霧坱埃",
+            "Thâu-tsîng bông-bū ing-ia",
+            "2"
+          ],
+          [
+            "",
+            "一粒心肝　猶算勇健",
+            "Tsi̍t lia̍p sim-kuann iá sǹg ióng-kiānn",
+            "2"
+          ],
+          [
+            "",
+            "有時猶是掠袂準　運命",
+            "Ū-sî iá sī lia̍h bē tsún ūn-miā",
+            "2"
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "客",
+            "$ 摎𠊎攬緪个海　<ruby>恁<rt>按</rt></ruby>闊",
+            "$ Láu ngǎi làm hěn ge hòi àn fàd ",
+            ""
+          ],
+          [
+            "",
+            "溫柔明亮月光　華華",
+            "Vún'iǔ mǐnliong ngiedgóng vǎvǎ",
+            ""
+          ],
+          [
+            "客+Tâi",
+            "離開个人無離開<span class='lang2'>（離開个儂無離開）</span>",
+            "Lǐkói ge ngǐn mǒ lǐkói <span class='lang2'>(Lī-khui ê lâng bô lī-khui)</span>",
+            ""
+          ],
+          [
+            "",
+            "像一節一節个火車<span class='lang2'>（若一節一節个火車）</span>",
+            "Qiong ìd jièd ìd jièd ge fòcá <span class='lang2'>(Ná tsi̍t tsiat tsi̍t tsiat ê hué-tshia)</span>",
+            ""
+          ],
+          [
+            "",
+            "佢一路一路送𫣆俚到這<span class='lang2'>（怹一路一路送咱到遮）</span>$",
+            "Gǐ ìd lu ìd lu sung énlì do ià <span class='lang2'>(In tsi̍t lōo tsi̍t lōo sàng lán kàu tsia)</span> $",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "Tâi",
+            "& 有路　咱沿路唱歌",
+            "Ū lōo lán iân-lōo tshiùnn-kua",
+            "2"
+          ],
+          [
+            "",
+            "無路　咱蹽溪過嶺",
+            "Bô lōo lán liâu khe kuè niá",
+            "2"
+          ],
+          [
+            "",
+            "時間　伊從來毋捌回頭行",
+            "Sî-kan i tsiông-lâi m̄-bat huê-thâu kiânn",
+            "2"
+          ],
+          [
+            "",
+            "毋敢向前莫來講輸贏",
+            "M̄-kánn hiòng-tsiân mài lâi kóng su-iânn",
+            "2"
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "無光　愛心內有數",
+            "Bô kng ài sim-lāi iú sòo",
+            "2"
+          ],
+          [
+            "",
+            "有光　伊必有緣故",
+            "Ū kng i pit iú iân-kòo",
+            "2"
+          ],
+          [
+            "",
+            "未來　<ruby>這<rt>即</rt></ruby>張無記號个地圖",
+            "Bī-lâi tsit tiunn bô kì-hō ê tē-tôo",
+            "2"
+          ],
+          [
+            "",
+            "親像咧叫我个 × 2",
+            "Tshin-tshiūnn leh kiò guá ê × 2",
+            "2"
+          ],
+          [
+            "",
+            "愛一步　一步 &",
+            "Ài tsi̍t pōo tsi̍t pōo",
+            "2"
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "*",
+            "",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "$",
+            "",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "&",
+            "",
+            ""
+          ]
+        ]
+      }
+    ]
+  }
+]
+
+names = {
+  "耆": ["'amis 耆老"],
+  "Tâi": ["Tâi-gí"],
+  "客": ["Hàgfa"],
+  "客+Tâi": ["重唱"],
+  "原": ["原民吟唱"]
+}
