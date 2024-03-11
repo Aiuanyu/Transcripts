1ae4b4a983959bcb1998746e276b067a5141a9d1
GHSRobert Ciang <Robertus0617@gmail.com>
2024-03-11 18:35:19 +0800

    3/2 臺灣翠青進度

---
+++ taiwan-green.js
@@ -0,0 +1,118 @@
+data = [
+  {
+    "EP": "",
+    "sections": [
+      {
+        "t": "臺灣翠青<br><br>詞 / 鄭兒玉<br>曲 / 蕭泰然",
+        "id": "taiwan-green",
+        "rows": [
+          [
+            "",
+            "太平洋西南海邊",
+            "Thài-pêng-iûⁿ se-lâm hái-piⁿ,",
+            "2"
+          ],
+          [
+            "",
+            "美麗島台灣翠青。",
+            "Bí-lē-tó Tâi-ôan chhùi-chhiⁿ.",
+            "2"
+          ],
+          [
+            "",
+            "早前受外邦統治，",
+            "Chá-chêng hō͘ gōa-pang thóng-tī,",
+            "2"
+          ],
+          [
+            "",
+            "<div class='sidenote'>有的版本用「獨立」，有的用「建國」，我取「建國」</div><span class='noted'>建國</span>今欲出頭天。",
+            "Kián-kok taⁿ teh chhut-thâu-thiⁿ.",
+            "2"
+          ],
+          [
+            "",
+            "共和國憲法的基礎，",
+            "Kiōng-hô-kok hiàn-hoat ê ki-chhó,",
+            "2"
+          ],
+          [
+            "",
+            "四族群平等相協助。",
+            "Sù-chok-kûn pêng-téng saⁿ hia̍p-chō͘.",
+            "2"
+          ],
+          [
+            "",
+            "人類文化、世界和平。",
+            "Jîn-lūi bûn-hòa sè-kài hô-pêng,",
+            "2"
+          ],
+          [
+            "",
+            "國民向前、貢獻才能。",
+            "Kok-bîn hiòng-chêng kòng-hiàn châi-lêng.",
+            "2"
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [
+            "",
+            "太平洋西南海邊",
+            "Taipǐn'iǒng xínǎm hòibién",
+            ""
+          ],
+          [
+            "",
+            "美麗島台灣翠鮮",
+            "Mílidò Tǒivǎn cuixién",
+            ""
+          ],
+          [
+            "",
+            "早時受外邦統治",
+            "Zòsǐi su ngoibáng tùngzii",
+            ""
+          ],
+          [
+            "",
+            "建國這下出頭天",
+            "Gienguèd liàha cùdtěu tién",
+            ""
+          ],
+          [
+            "",
+            "共和國憲法个基礎",
+            "Kiungfǒguèd hienfàb ke gícù",
+            ""
+          ],
+          [
+            "",
+            "各族群平等相協助",
+            "Gòg cugkiǔn pǐndéǹ siong hiàbcu",
+            ""
+          ],
+          [
+            "",
+            "人類文化 世界和平",
+            "Ngǐnlui mǔnfa siigie fǒpǐn",
+            ""
+          ],
+          [
+            "",
+            "國民向前 貢獻才情",
+            "Guèdmǐn hiong qiěn gunghien cǒiqǐn",
+            ""
+          ]
+        ]
+      }
+    ]
+  }
+]
+
+names = {
+}
